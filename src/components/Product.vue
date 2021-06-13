e6b359a8665d7cffeba398c28ccafbd7577fa12b
steeltx <realsteel.gnu@gmail.com>
2021-06-13 18:49:41 -0500

    Mostrando productos

---
+++ src/components/Product.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{product.name}}</p>
+</template>
+
+<script>
+export default {
+  name:"Product",
+  props:{
+    product: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <BasicLayout>
-    <h2>Estamos en home</h2>
+    <h1>Últimos productos</h1>
+    <div class="ui grid">
+      <div
+        class="sixten wide mobile eight wide tablet four wide compuer column"
+        v-for="product in products"
+        :key="product.id"
+      >
+       <Product :product="product" />
+      </div>
+    </div>
   </BasicLayout>
 </template>
 
@@ -8,21 +17,23 @@
 import { ref, onMounted } from "vue";
 import BasicLayout from "../layouts/BasicLayout.vue";
 import { getProducts } from "../api/product";
+import Product from "../components/Product.vue";
 
 export default {
   name: "Home",
   components: {
     BasicLayout,
+    Product,
   },
   setup() {
-    let producs = ref(null);
+    let products = ref(null);
 
     onMounted(async () => {
       const response = await getProducts(20);
-      producs.value = response;
+      products.value = response;
     });
     return {
-      producs,
+      products,
     };
   },
 };
