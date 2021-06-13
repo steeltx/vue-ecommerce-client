703fff37a05d6c3915cdc285d8fc64c47d253165
steeltx <realsteel.gnu@gmail.com>
2021-06-13 00:16:55 -0500

    Estructura form registro usuarios

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,23 @@
 <template>
   <BasicLayout>
-    <h1>Registro</h1>
-    <router-link to="/login"> Iniciar sesión </router-link>
+    <div class="register">
+      <h2>Registro de usuario</h2>
+      <form class="ui form">
+        <div class="field">
+          <input type="text" placeholder="Nombre de usuario" />
+        </div>
+        <div class="field">
+          <input type="text" placeholder="Correo" />
+        </div>
+        <div class="field">
+          <input type="password" placeholder="Contraseña" />
+        </div>
+        <button type="submit" class="ui button fluid primary">
+          Crear usuario
+        </button>
+      </form>
+      <router-link to="/login"> Iniciar sesión </router-link>
+    </div>
   </BasicLayout>
 </template>
 
@@ -15,5 +31,22 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.register {
+  text-align: center;
+
+  > h2 {
+    margin: 50px 0 30px 0;
+  }
+
+  .ui.form {
+    max-width: 300px !important;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    input.error {
+      border-color: #faa;
+      background-color: #ffeded;
+    }
+  }
+}
 </style>
