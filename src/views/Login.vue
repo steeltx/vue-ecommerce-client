a8fb7cb0001d3dcdafe2af3129ebcb3749d0c5cc
steeltx <realsteel.gnu@gmail.com>
2021-06-13 00:10:56 -0500

    Paginas iniciales para login y registro

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <BasicLayout>
+    <h1>Login</h1>
+    <router-link to="/register"> Crear una cuenta </router-link>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "../layouts/BasicLayout.vue";
+export default {
+  name: "Login",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <BasicLayout>
+    <h1>Registro</h1>
+    <router-link to="/login"> Iniciar sesión </router-link>
+  </BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "../layouts/BasicLayout.vue";
+export default {
+  name: "Register",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style>
+</style>
