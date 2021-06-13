c1a41c5fb63af278c2c60a2741c0d06fae01fe4d
steeltx <realsteel.gnu@gmail.com>
2021-06-13 16:30:37 -0500

    Ruta dinamica para categorias

---
+++ src/views/Category.vue
@@ -0,0 +1,24 @@
+<template>
+<BasicLayout>
+    <h1>Estamos en la categoria</h1>
+</BasicLayout>
+</template>
+
+<script>
+import BasicLayout from "../layouts/BasicLayout.vue";
+
+export default {
+  name: "Category",
+  components: {
+    BasicLayout,
+  },
+  watch: {
+      $route(to, from){
+          console.log(to);
+      }
+  }
+};
+</script>
+
+<style>
+</style>
