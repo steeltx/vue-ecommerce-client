7da4dae699e6e73f4b6225026b9f81ae6a2e0963
steeltx <realsteel.gnu@gmail.com>
2021-06-13 20:14:01 -0500

    Miltiples productos en carrito

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,7 +1,13 @@
 import { API_URL, PRODUCTS } from "../utils/constans";
 
 export function addProductCartApi(idProduct) {
-    const products = [];
+    const products = getCartApi();
     products.push(idProduct);
     localStorage.setItem(PRODUCTS, JSON.stringify(products));
+}
+
+export function getCartApi() {
+    const products = localStorage.getItem(PRODUCTS);
+    if (!products) return [];
+    return JSON.parse(products);
 }
