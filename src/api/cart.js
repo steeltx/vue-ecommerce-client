f51b80b85aee64545c0621fac2d3d339d2d9171a
steeltx <realsteel.gnu@gmail.com>
2021-06-16 00:26:27 -0500

    Obtener productos de carrito

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,3 +1,4 @@
+import { uniqBy, countBy } from "lodash";
 import { API_URL, PRODUCTS } from "../utils/constans";
 
 export function addProductCartApi(idProduct) {
@@ -10,4 +11,34 @@ export function getCartApi() {
     const products = localStorage.getItem(PRODUCTS);
     if (!products) return [];
     return JSON.parse(products);
+}
+
+export async function getProductsCartApi() {
+    // obtener todos los id del producto
+    const idProducts = getCartApi();
+    if (idProducts.lenght === 0) return null;
+    try {
+        const products = [];
+        for await (const idProduct of idProducts) {
+            const response = await fetch(`${API_URL}/producs/${idProduct}`);
+            const result = await response.json();
+            products.push(result);
+        }
+
+        // contar cuantos productos hay iguales
+        const productsCount = countBy(products, (product) => {
+            return product.name;
+        });
+
+        // obtener los productos unicos, y contar el total en el array
+        const combined = uniqBy(products, (product) => {
+            const productTemp = product;
+            productTemp.quantity = productsCount[product.name];
+            return productTemp.name;
+        });
+        return combined;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -8,9 +8,10 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { ref, computed, watchEffect } from "vue";
 import { useStore } from "vuex";
 import CartHeader from "./CartHeader.vue";
+import { getProductsCartApi } from "../../api/cart";
 
 export default {
   name: "Cart",
@@ -20,6 +21,17 @@ export default {
   setup() {
     const store = useStore();
     const showCart = computed(() => store.state.showCart);
+    let products = ref(null);
+
+    const getProductsCart = async () => {
+      const response = await getProductsCartApi();
+      products.value = response;
+    };
+
+    watchEffect(() => {
+      showCart.value;
+      getProductsCart();
+    });
 
     const closeCart = () => {
       store.commit("setShowCart", false);
