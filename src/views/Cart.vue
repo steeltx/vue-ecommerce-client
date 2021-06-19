030ed7225bfbf6cc6f6386979ad4e7fb0619f261
steeltx <realsteel.gnu@gmail.com>
2021-06-19 12:17:39 -0500

    Obtener total y mostrarlo en tabla

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -2,32 +2,32 @@
   <BasicLayout>
     <h1>Carrito</h1>
     <table class="ui celled table" v-if="products">
-        <thead>
-            <tr>
-                <th>Producto</th>
-                <th>Cantidad</th>
-                <th>Precio</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="product in products" :key="product.id">
-                <td>{{product.name}}</td>
-                <td>{{product.quantity}}</td>
-                <td> $ {{product.price}}</td>
-                <td style="text-align:center">
-                    <i class="close icon"></i>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Total :</td>
-                <td colspan="2"></td>
-            </tr>
-        </tbody>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Cantidad</th>
+          <th>Precio</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.name }}</td>
+          <td>{{ product.quantity }}</td>
+          <td>$ {{ product.price }}</td>
+          <td style="text-align: center">
+            <i class="close icon"></i>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Total :</td>
+          <td colspan="2">$ {{ getTotal() }}</td>
+        </tr>
+      </tbody>
     </table>
     <button class="ui button primary fluid" v-if="products.length > 0">
-        Generar pedido
+      Generar pedido
     </button>
     <h3 v-if="products.length < 1">No tienes productos en el carrito</h3>
   </BasicLayout>
@@ -51,9 +51,18 @@ export default {
       products.value = response;
     });
 
+    const getTotal = () => {
+      let totalTemp = 0;
+      products.value.forEach((product) => {
+        totalTemp += product.price * product.quantity;
+      });
+      return totalTemp.toFixed(2);
+    };
+
     return {
       products,
-    }
+      getTotal,
+    };
   },
 };
 </script>
